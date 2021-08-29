2c6baf8a6ef175157d76afe143eda697d1f58acc
chagy <chagy_x_@hotmail.com>
2021-08-29 19:10:19 +0700

    Complete project

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -2,7 +2,9 @@
   import { onMount } from "svelte";
   import { navigate, link } from "svelte-routing";
   import user from "../stores/user";
-  import { cartTotal } from "../stores/cart";
+  import cart, { cartTotal } from "../stores/cart";
+  import submitOrder from "../strapi/submitOrder";
+  import globalStore from "../stores/globalStore";
 
   let name = "";
   let cardElement;
@@ -11,15 +13,64 @@
   let stripe;
   let elements;
 
-  $: isEmpty = !name;
+  $: isEmpty = !name || $globalStore.alert;
 
   onMount(() => {
     if (!$user.jwt) {
       navigate("/");
+      return;
+    }
+
+    if ($cartTotal > 0) {
+      stripe = Stripe(
+        "pk_test_51H9Y5fK4vC2b1QpuA6jdIG2vAdQpfAZU84cFuO3kVNhGH6iO7JOpHprYFKM7Wdv7ynIozlz3lcw5w8YsS0Wlu9Dv00VoOqDdVh"
+      );
+      elements = stripe.elements();
+      card = elements.create("card");
+      card.mount(cardElement);
+      card.addEventListener("change", function (event) {
+        if (event.error) {
+          cardErrors.textContent = event.error.message;
+        } else {
+          cardErrors.textContent = "";
+        }
+      });
     }
   });
 
-  function handleSubmit() {}
+  async function handleSubmit() {
+    globalStore.toggleItem("alert", true, "submitting order... please wait?");
+    let response = await stripe
+      .createToken(card)
+      .catch((error) => console.log(error));
+
+    const { token } = response;
+    if (token) {
+      const { id } = token;
+      let order = await submitOrder({
+        name,
+        total: $cartTotal,
+        items: $cart,
+        stripeTokenId: id,
+        userToken: $user.jwt,
+      });
+
+      if (order) {
+        globalStore.toggleItem("alert", true, "your order is complete");
+        cart.set([]);
+        localStorage.setItem("cart", JSON.stringify([]));
+        navigate("/");
+        return;
+      }
+    } else {
+      globalStore.toggleItem(
+        "alert",
+        true,
+        "there error please try agin",
+        true
+      );
+    }
+  }
 </script>
 
 {#if $cartTotal > 0}
